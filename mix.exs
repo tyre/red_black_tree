c04b1a62aa393245a4704823289c32ac8601627e
Chris Schneider <chris@scoutapp.com>
2017-05-03 10:52:00 -0600

    Fix Elixir1.4 warnings in mix.exs

--- mix.exs
+++ mix.exs
@@ -9,8 +9,8 @@ defmodule RedBlackTree.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
